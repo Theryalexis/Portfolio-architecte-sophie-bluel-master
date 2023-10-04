77630044eb3a9b56929cd56bbe061fe788b086e7
Theryalexis <alexis62330@hotmail.fr>
2023-10-04 09:37:53 +0200

    creation filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,5 @@
+//import images back-end//
+
 const imagesContainer = document.querySelector('.gallery')
 
 function createWorkFigure(work) {
@@ -26,3 +28,48 @@ fetch('http://localhost:5678/api/works')
     });
   });
 
+//filtres//
+
+function filtreobjet(){
+    const element = document.querySelectorAll('div.gallery figure');
+    elements.forEach((element)=>{
+        const categoryId = element.getAttribute('category-id');
+        if (categoryId === '1'){
+            element.style.display = 'block';
+        }else {
+            element.style.display = 'none';
+        }
+    });
+}
+
+var bouton = document.getElementById('btnObjet');
+bouton.addEventListener('click',filtreobjet);
+
+function filtreappartement () {
+    const element = document.querySelectorAll ('div.gallery figure');
+    elements.forEach((element)=>{
+        const categoryId = element.getAttribute('category-id');
+        if (categoryId === '2'){
+            element.style.display = 'block';
+        }else {
+            element.style.display = 'none';
+        }
+    });
+}
+
+var bouton = document.getElementById('btnAppartement');
+bouton.addEventListener('click',filtreappartement);
+
+function filtrehotel (){
+    const element = document.querySelectorAll('div.gallery figure');
+    elements.forEach((element)=>{
+        const categoryId = element.getAttribute('category-id');
+        if (categoryId === '3'){
+            element.style.display = 'block';
+        }else {
+            element.style.display = 'none';
+        }
+})
+}
+var bouton = document.getElementById('btnHotelRestaurant');
+bouton.addEventListener('click' filtrehotel);
