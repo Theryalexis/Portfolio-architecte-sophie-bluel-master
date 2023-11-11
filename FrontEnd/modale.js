32f23826740abf5b975be7b61454c6b9d3bab42a
Theryalexis <alexis62330@hotmail.fr>
2023-11-11 14:03:05 +0100

    Modale1

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -1,6 +1,9 @@
 const token = sessionStorage.getItem("token");
 const editorblack = document.querySelector(".editor-black");
 const button_modifier = document.querySelector(".button-modifier");
+const button_editor = document.getElementById("button_editor");
+const modale1 = document.querySelector(".container_modale1");
+const modale1_fermer = document.querySelector(".modale1_cross");
 let loginout;
 
 function logout() {
@@ -18,5 +21,15 @@ document.body.onload = async function () {
     loginout.addEventListener("click", logout);
     button_modifier.classList.remove("display_none"); // On enlève le display none au bouton modifier pour le rendre visible //
     loginout.setAttribute("href", "./index.html");
+    button_editor.addEventListener("click", display_modale1);
+    modale1_fermer.addEventListener("click", fermeture_modale1);
   }
 };
+//fonction pour afficher la modale 1
+function display_modale1() {
+  modale1.classList.remove("display_none");
+}
+//Fonction pour fermet la modale 1
+function fermeture_modale1() {
+  modale1.classList.add("display_none");
+}
