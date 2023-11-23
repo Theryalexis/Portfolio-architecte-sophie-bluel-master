8ef1fa65d19bd6f38b6f4f9e9b071f92a5400a5d
Theryalexis <alexis62330@hotmail.fr>
2023-11-23 18:46:11 +0100

    Rectification categorie via api

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,97 +1,11 @@
-const btnAll = document.querySelector(".filter__btn-id-null");
-const btnId1 = document.querySelector(".filter__btn-id-1");
-const btnId2 = document.querySelector(".filter__btn-id-2");
-const btnId3 = document.querySelector(".filter__btn-id-3");
-
+const btnfilters = document.querySelector(".filters");
 const sectionProjets = document.querySelector(".gallery");
 const gallery_modale = document.querySelector(".modale1_gallery");
-
-let data = null;
-let id;
-generationProjets(data, null);
 
 // Reset la section projets
 function resetSectionProjets() {
   sectionProjets.innerHTML = "";
 }
-
-// Génère les projets
-async function generationProjets(data, id) {
-  try {
-    const response = await fetch("http://localhost:5678/api/works");
-    data = await response.json();
-  } catch {
-    const p = document.createElement("p");
-    p.classList.add("error");
-    p.innerHTML =
-      "Une erreur est survenue lors de la récupération des projets<br><br>Une tentative de reconnexion automatique auras lieu dans une minute<br><br><br><br>Si le problème persiste, veuillez contacter l'administrateur du site";
-    sectionProjets.appendChild(p);
-    await new Promise((resolve) => setTimeout(resolve, 60000));
-    window.location.href = "index.html";
-  }
-
-  resetSectionProjets();
-
-  // Filtre les résultats
-  if ([1, 2, 3].includes(id)) {
-    data = data.filter((data) => data.categoryId == id);
-  }
-
-  // Change la couleur du bouton en fonction du filtre
-  document.querySelectorAll(".filter__btn").forEach((btn) => {
-    btn.classList.remove("filter__btn--active");
-  });
-  document
-    .querySelector(`.filter__btn-id-${id}`)
-    .classList.add("filter__btn--active");
-
-  if (data.length === 0 || data === undefined) {
-    const p = document.createElement("p");
-    p.classList.add("error");
-    p.innerHTML =
-      "Aucun projet à afficher <br><br>Toutes nos excuses pour la gêne occasionnée";
-    sectionProjets.appendChild(p);
-    return;
-  }
-
-  // Génère les projets
-  if (id === null || [1, 2, 3].includes(id)) {
-    for (let i = 0; i < data.length; i++) {
-      const figure = document.createElement("figure");
-      sectionProjets.appendChild(figure);
-      const img = document.createElement("img");
-      img.src = data[i].imageUrl;
-      img.alt = data[i].title;
-      figure.appendChild(img);
-
-      const figcaption = document.createElement("figcaption");
-      figcaption.innerHTML = data[i].title;
-      figure.appendChild(figcaption);
-    }
-  }
-}
-
-// >>> FILTRES
-
-btnAll.addEventListener("click", () => {
-  // Tous les projets
-  generationProjets(data, null);
-});
-
-btnId1.addEventListener("click", () => {
-  // Objets
-  generationProjets(data, 1);
-});
-
-btnId2.addEventListener("click", () => {
-  // Appartements
-  generationProjets(data, 2);
-});
-
-btnId3.addEventListener("click", () => {
-  // Hôtels & restaurants
-  generationProjets(data, 3);
-});
 
 /*-----------------------------------*/
 
@@ -142,3 +56,78 @@ async function trashcanClicked(event) {
   generationProjetsmodale();
   generationProjets(data, null);
 }
+//fonction qui gere l'affichage des bouton via l'api
+async function getcategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  let categories = await response.json();
+  const bouttonfiltre = document.querySelector(".filters");
+  let nbCategories = categories.length;
+
+  // Ajouter le bouton "Tous"
+  const btnTous = document.createElement("button");
+  btnTous.classList.add("filter__btn", "all", "filter__btn--active"); // Ajoutez la classe "filter__btn--active" ici
+  btnTous.innerHTML = "Tous";
+  bouttonfiltre.appendChild(btnTous);
+
+  for (let i = 0; i < nbCategories; i++) {
+    const btnfiltres = document.createElement("button");
+    btnfiltres.classList.add("filter__btn");
+    btnfiltres.innerHTML = categories[i].name;
+    bouttonfiltre.appendChild(btnfiltres);
+    bouttonfiltre.addEventListener("click", buttonClicked);
+  }
+
+  bouttonfiltre.addEventListener("click", function (event) {
+    const buttons = document.querySelectorAll(".filter__btn");
+    buttons.forEach((button) => {
+      button.classList.remove("filter__btn--active");
+    });
+
+    if (event.target.classList.contains("filter__btn")) {
+      event.target.classList.add("filter__btn--active");
+      buttonClicked(event.target.innerHTML);
+    } else if (event.target.classList.contains("all")) {
+      // Gérer le clic sur le bouton "Tous"
+      event.target.classList.add("filter__btn--active");
+    }
+  });
+}
+
+getcategories();
+
+//fonction qui affiche les projets via les filtres
+function buttonClicked(event) {
+  let worksFiltered = works.filter(function (work) {
+    if (event.target.innerText === "Tous") {
+      return true;
+    }
+    return work.category.name === event.target.innerText;
+  });
+  createProjectsCards(worksFiltered);
+}
+
+function createProjectsCards(worksFiltered) {
+  const gallery = document.querySelector(".gallery");
+  let nbProjects = worksFiltered.length;
+  resetSectionProjets();
+  for (let i = 0; i < nbProjects; i++) {
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    image.setAttribute("src", worksFiltered[i].imageUrl);
+    image.setAttribute("alt", worksFiltered[i].title);
+    figcaption.innerHTML = worksFiltered[i].title;
+    figure.appendChild(image);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  }
+}
+async function createProjects() {
+  await getWorks();
+  createProjectsCards(works);
+}
+async function getWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  works = await response.json();
+}
+createProjects();
